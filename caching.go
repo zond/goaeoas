d34fdd56a7952e11b297d9fa3a62ee8250a4d626
Martin Bruse <zondolfin@gmail.com>
2020-12-02 12:00:28 +0100

    Made the ETag caching differentiate on accept settings.

--- caching.go
+++ caching.go
@@ -1,6 +1,8 @@
 package goaeoas
 
 import (
+	"crypto/sha1"
+	"fmt"
 	"net/http"
 
 	"google.golang.org/appengine"
@@ -8,7 +10,10 @@ import (
 
 func VersionETagCache(handler func(ResponseWriter, Request) error) func(ResponseWriter, Request) error {
 	return func(w ResponseWriter, r Request) error {
-		etag := appengine.VersionID(appengine.NewContext(r.Req()))
+		media, charset := Media(r.Req(), "Accept")
+		h := sha1.New()
+		h.Write([]byte(fmt.Sprintf("version:%s,media:s,charset:s", appengine.VersionID(appengine.NewContext(r.Req())), media, charset)))
+		etag := fmt.Sprintf("%x", h.Sum(nil))
 		if r.Req().Header.Get("If-None-Match") == etag {
 			w.WriteHeader(http.StatusNotModified)
 			return nil
