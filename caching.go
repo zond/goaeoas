f9fa928cfbfdb4d5a77a97a55e73266b374c3544
Martin Bruse <zondolfin@gmail.com>
2020-12-02 12:41:06 +0100

    Made the ETag weak, hoping it will pass stricter proxies.

--- caching.go
+++ caching.go
@@ -13,7 +13,7 @@ func VersionETagCache(handler func(ResponseWriter, Request) error) func(Response
 		media, charset := Media(r.Req(), "Accept")
 		h := sha1.New()
 		h.Write([]byte(fmt.Sprintf("version:%s,media:s,charset:s", appengine.VersionID(appengine.NewContext(r.Req())), media, charset)))
-		etag := fmt.Sprintf("%x", h.Sum(nil))
+		etag := fmt.Sprintf("W/%x", h.Sum(nil))
 		if r.Req().Header.Get("If-None-Match") == etag {
 			w.WriteHeader(http.StatusNotModified)
 			return nil
